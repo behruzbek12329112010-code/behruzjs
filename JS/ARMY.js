5402af5c2101f33d378401fff9d6ffcb8fe43e43
behruzbek12329112010-code <behruz12329112010@gmail.com>
2026-02-05 15:58:46 +0500

    jso'zgaruvchi

---
+++ JS/ARMY.js
@@ -0,0 +1,13 @@
+var name = prompt("enter name?").trim();
+var svg = prompt("age").trim();
+var jpg = prompt("jins").trim();
+var png = prompt("armiyaga-borganmisiz").trim();
+console.log(name, age, jinsingiz, armiyaga-borganmisiz);
+
+
+let gander = prompt("enter gander").trim();
+if (png == "man") {
+    let gander = prompt("armiyaga borganmisz").trim();
+}else{
+    console.log(raxmat);
+}
